83fd9cc55e967a1d5b9d58ccd81be9f30539a220
Inder <inder@unknown.home>
2018-12-01 03:23:29 +1100

    created game Logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,17 @@
-var cardOne = "King";
-var cardTwo = "King";
-var cardThree = "Queen";
-var cardFour = "Queen";
+var cards = ["Queen","Queen","King","King"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
 
-console.log("User flipped " + cardOne);
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+//console.log(cardsInPlay[1]);
+if(cardsInPlay.length===2)
+{
+	if (cardsInPlay[0]===cardsInPlay[1]) 
+	{
+		alert("You found a match !");
+	}
+	alert("Sorry, Try again.");
+}
