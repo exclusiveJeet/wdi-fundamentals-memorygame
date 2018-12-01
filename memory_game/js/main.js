cb41d30ae71818e6d9cfe2dfbb974c56af3e4e69
Inder <inder@unknown.home>
2018-12-01 23:21:13 +1100

    created game Logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,28 @@
-var cards = ["Queen","Queen","King","King"];
+var cards = [
+			  	{
+			  	rank : "Queen" ,
+			    suit : "Hearts",
+			    cardImage : "images/queen-of-hearts.png"
+				},
+
+				{
+			  	rank : "Queen" ,
+			    suit : "Diamonds",
+			    cardImage : "images/queen-of-diamonds.png"
+				},
+
+				{
+			  	rank : "King" ,
+			    suit : "Hearts",
+			    cardImage : "images/king-of-hearts.png"
+				},
+
+				{
+			  	rank : "King" ,
+			    suit : "Diamonds",
+			    cardImage : "images/king-of-diamonds.png"
+				}
+			];
 var cardsInPlay = [];
 
 
@@ -18,8 +42,10 @@ var checkForMatch = function()
 var flipCard = function(cardID)
 {
 
-	console.log("User flipped " + cards[cardID]);
-	cardsInPlay = cards[cardID];
+	console.log("User flipped " + cards[cardID].rank);
+	cardsInPlay.push(cards[cardID].rank);
+
+	console.log(cards[cardID].rank + " - "+cards[cardID].cardImage);
 	checkForMatch();
 }
 
